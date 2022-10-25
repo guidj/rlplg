5287b20fc289cbee7a318b967175d5f88e6313c7
guilherme <guilherme@spotify.com>
2022-10-25 19:49:58 +0200

    Bump version: 0.10.2 → 0.10.3

--- src/rlplg/__init__.py
+++ src/rlplg/__init__.py
@@ -1,5 +1,5 @@
 __author__ = "guilherme"
-__version__ = "0.10.2"
+__version__ = "0.10.3"
 __email__ = "guilherme@dsv.su.se"
 __description__ = "RL-Playground"
 __uri__ = "https://github.com/guidj/rlplg"
