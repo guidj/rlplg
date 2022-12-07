0840264f12998ed5832de07abe29f341332730be
guilherme <guilherme@spotify.com>
2022-12-07 18:22:05 +0100

    Refactors learning rate schedule
      - Add initial rate parameter
      - Removes epoch schedule - logic can reside in learning rate schedule
        `schedule function`

--- tests/rlplg/learning/opt/test_schedules.py
+++ tests/rlplg/learning/opt/test_schedules.py
@@ -1,54 +1,37 @@
-import math
-
 from rlplg.learning.opt import schedules
 
 
-def test_epoch_schedule_call():
-    def schedule(step: int):
-        assert step >= 0
-        return math.floor(step // 100)
-
-    epoch_scheduler = schedules.EpochSchedule(schedule=schedule)
-
-    output_1 = epoch_scheduler(step=1)
-    output_2 = epoch_scheduler(step=2)
-    output_3 = epoch_scheduler(step=100)
-    output_4 = epoch_scheduler(step=202)
-    output_5 = epoch_scheduler(step=304)
-
-    assert output_1 == 0
-    assert output_2 == 0
-    assert output_3 == 1
-    assert output_4 == 2
-    assert output_5 == 3
-
-
 def test_learning_rate_schedule_call():
-    lrs = schedules.LearningRateSchedule(
-        schedule=lambda _, learning_rate: learning_rate * 0.9
-    )
+    def schedule(initial_learning_rate: float, step: int):
+        return initial_learning_rate * (0.9**step)
 
-    output_1 = lrs(step=1, learning_rate=1)
-    output_2 = lrs(step=2, learning_rate=1)
-    output_3 = lrs(step=2, learning_rate=100)
+    output_1 = schedules.LearningRateSchedule(
+        initial_learning_rate=1.0, schedule=schedule
+    )(step=1)
+    output_2 = schedules.LearningRateSchedule(
+        initial_learning_rate=1.0, schedule=schedule
+    )(step=2)
+    output_3 = schedules.LearningRateSchedule(
+        initial_learning_rate=100.0, schedule=schedule
+    )(step=2)
 
     assert output_1 == 0.9
-    assert output_2 == 0.9  # epoch doesn't affect output
-    assert output_3 == 90
+    assert output_2 == 0.81  # epoch doesn't affect output
+    assert output_3 == 81
 
 
 def test_learning_rate_schedule_call_using_epoch_based_scheduler():
-    def schedule(step: int, learning_rate: float):
+    def schedule(initial_learning_rate: float, step: int):
         if step < 10:
-            return learning_rate
-        return learning_rate * 0.9
+            return initial_learning_rate
+        return initial_learning_rate * 0.9
 
-    lrs = schedules.LearningRateSchedule(schedule=schedule)
+    lrs = schedules.LearningRateSchedule(initial_learning_rate=1.0, schedule=schedule)
 
-    output_1 = lrs(step=1, learning_rate=1)
-    output_2 = lrs(step=9, learning_rate=1)
-    output_3 = lrs(step=10, learning_rate=1)
-    output_4 = lrs(step=21, learning_rate=1)
+    output_1 = lrs(step=1)
+    output_2 = lrs(step=9)
+    output_3 = lrs(step=10)
+    output_4 = lrs(step=21)
 
     assert output_1 == 1
     assert output_2 == 1
