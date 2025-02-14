1c9a79024c7e28152ededf458f49f6e18905d530
Guilherme <guilherme@spotify.com>
2025-02-14 22:32:30 +0100

    fix: revert version

--- src/rlplg/__init__.py
+++ src/rlplg/__init__.py
@@ -1,5 +1,5 @@
 __author__ = "guilherme"
-__version__ = "1.1.1"
+__version__ = "0.20.3"
 __email__ = "guilherme@dsv.su.se"
 __description__ = "RL-Playground"
 __uri__ = "https://github.com/guidj/rlplg"
